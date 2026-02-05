81a9ac4f8f56dad0e551254844ce24fd5545330c
joseluisth01 <124920964+joseluisth01@users.noreply.github.com>
2026-02-05 17:57:31 +0100

    grewerg

--- app/Libraries/Pdf.php
+++ app/Libraries/Pdf.php
@@ -4,21 +4,21 @@ namespace App\Libraries;
 
 require_once APPPATH . "ThirdParty/tcpdf/tcpdf.php";
 
-class Pdf extends \TCPDF {
-
+class Pdf extends \TCPDF
+{
     private $pdf_type;
 
-    public function __construct($pdf_type = '') {
+    public function __construct($pdf_type = '')
+    {
         parent::__construct(PDF_PAGE_ORIENTATION, PDF_UNIT, PDF_PAGE_FORMAT, true, 'UTF-8', false);
-
         $this->pdf_type = $pdf_type;
         $this->SetFontSize(10);
         $this->setListIndentWidth(7);
         $this->setImageScale(1.42);
     }
 
-    public function Header() {
-
+    public function Header()
+    {
         if ($this->pdf_type == 'invoice') {
             $break_margin = $this->getBreakMargin();
             $auto_page_break = $this->AutoPageBreak;
@@ -40,8 +40,8 @@ class Pdf extends \TCPDF {
         }
     }
 
-    public function PreparePDF($content, $pdf_file_name,  $mode = "download", $is_mobiel_preview = false) {
-
+    public function PreparePDF($content, $pdf_file_name, $mode = "download", $is_mobiel_preview = false)
+    {
         if (!$content) {
             return;
         }
@@ -64,7 +64,6 @@ class Pdf extends \TCPDF {
 
         $this->writeHTML($content, true, false, true, false, '');
 
-
         $pdf_file_name = get_hyphenated_string($pdf_file_name) . ".pdf";
 
         if ($mode === "download") {
@@ -74,11 +73,11 @@ class Pdf extends \TCPDF {
             $this->Output($temp_download_path, "F");
             return $temp_download_path;
         } else if ($mode === "view") {
-
             if ($is_mobiel_preview) {
                 $this->SetTitle($pdf_file_name);
-                $pdf_content = $this->Output($pdf_file_name, "S");  // Get PDF content as a variable
-
+                $pdf_content = $this->Output($pdf_file_name, "S");
+                
+                // Get PDF content as a variable
                 echo '<div class="app-modal">';
                 echo '<div class="app-modal-content">';
                 echo '<iframe id="iframe-file-viewer" src="data:application/pdf;base64,' . base64_encode($pdf_content) . '" width="100%" height="100%" style="border: none;"></iframe>';
@@ -92,13 +91,15 @@ class Pdf extends \TCPDF {
         }
     }
 
-    public function writeHTML($html, $ln = true, $fill = false, $reseth = false, $cell = false, $align = '') {
+    public function writeHTML($html, $ln = true, $fill = false, $reseth = false, $cell = false, $align = '')
+    {
         $html = $this->_convert_image_links($html);
         $html = $this->_rebuild_html($html);
         parent::writeHTML($html, $ln, $fill, $reseth, $cell, $align);
     }
 
-    private function _convert_image_links($content) {
+    private function _convert_image_links($content)
+    {
         //if (get_setting('only_file_path')) {
         $base_url = base_url();
         $assets_path = $base_url . "assets";
@@ -108,32 +109,26 @@ class Pdf extends \TCPDF {
         return $content;
     }
 
-    private function _rebuild_html($content) {
-
-        // Add cellpadding to <table> tags 
+    private function _rebuild_html($content)
+    {
+        // Add cellpadding to <table> tags
         $cellpadding = 10;
         $content = preg_replace_callback('/<table(.*?)>/i', function ($matches) use ($cellpadding) {
             $attributes = $matches[1];
-
             // Check if cellpadding is already set
             if (!preg_match('/\bcellpadding\s*=\s*["\']?\d+["\']?/i', $attributes)) {
                 $attributes .= ' cellpadding="' . $cellpadding . '"';
             }
-
             return '<table' . $attributes . '>';
         }, $content);
 
-
         // Add line-height to <li>
         $li_line_height = 1.5;
-
         $content = preg_replace_callback('/<li(.*?)>/i', function ($matches) use ($li_line_height) {
             $attributes = $matches[1];
-
             if (preg_match('/\bstyle\s*=\s*["\'](.*?)["\']/i', $attributes, $style_match)) {
                 // Extract existing styles
                 $existing_styles = $style_match[1];
-
                 // Add line-height if not already present
                 if (!preg_match('/\bline-height\s*:\s*[\d.]+(px|em|rem|%)?\b/i', $existing_styles)) {
                     $newStyles = rtrim($existing_styles, ';') . '; line-height:' . $li_line_height . ';';
@@ -143,21 +138,15 @@ class Pdf extends \TCPDF {
                 // No style attribute, add it
                 $attributes .= ' style="line-height:' . $li_line_height . ';"';
             }
-
             return '<li' . $attributes . '>';
         }, $content);
 
-
-
         $ul_line_height = 0.2;
-
         $content = preg_replace_callback('/<ul(.*?)>/i', function ($matches) use ($ul_line_height) {
             $attributes = $matches[1];
-
             if (preg_match('/\bstyle\s*=\s*["\'](.*?)["\']/i', $attributes, $style_match)) {
                 // Extract existing styles
                 $existing_styles = $style_match[1];
-
                 // Add line-height if not already present
                 if (!preg_match('/\bline-height\s*:\s*[\d.]+(px|em|rem|%)?\b/i', $existing_styles)) {
                     $newStyles = rtrim($existing_styles, ';') . '; line-height:' . $ul_line_height . ';';
@@ -167,10 +156,8 @@ class Pdf extends \TCPDF {
                 // No style attribute, add it
                 $attributes .= ' style="line-height:' . $ul_line_height . '; padding-bottom: 0px;"';
             }
-
             return '<ul' . $attributes . '>';
         }, $content);
-
 
         //change paragraph line height
         $content = preg_replace_callback('/<p\b([^>]*)>/i', function ($matches) {
@@ -193,32 +180,26 @@ class Pdf extends \TCPDF {
         }, $content);
 
         // Wrap <hr> inside <p> while preserving attributes
-        $content =  preg_replace_callback('/<hr(.*?)>/i', function () {
+        $content = preg_replace_callback('/<hr(.*?)>/i', function () {
             return '<p><hr style="color:#f2f4f6;"></p>';
         }, $content);
 
-
         $page_width = $this->getPageWidth();
-
         $page_width_in_px = ($page_width / 25.4) * 92;
 
         // Replace percentage-based styles with pixel-based styles
         $content = preg_replace_callback('/style=["\'](.*?)["\']/i', function ($matches) use ($page_width_in_px) {
             $style = $matches[1];
-
             // Replace percentage-based width with pixel-based width
             $style = preg_replace_callback('/width\s*:\s*(\d+%)/i', function ($width_matches) use ($page_width_in_px) {
                 $percentage_width = $width_matches[1];
                 $pixelWidth = $page_width_in_px * (floatval($percentage_width) / 100);
                 return 'width: ' . $pixelWidth . 'px';
             }, $style);
-
             return 'style="' . $style . '"';
         }, $content);
 
-
         $rem_base = 14; // 1rem = 14px for pdf
-
         $content = preg_replace_callback('/font-size\s*:\s*([\d.]+)\s*rem/i', function ($matches) use ($rem_base) {
             $rem_value = (float) $matches[1];
             $px_value = $rem_value * $rem_base;
@@ -226,13 +207,13 @@ class Pdf extends \TCPDF {
         }, $content);
 
         $default_style = "<style>
-                h1 { font-size: 32px; font-weight: normal; }
-                h2 { font-size: 28px; font-weight: normal; }
-                h3 { font-size: 24px; font-weight: normal; }
-                h4 { font-size: 18px; font-weight: normal; }
-                h5 { font-size: 17px; }
-                h6 { font-size: 14px; font-weight: normal; }
-                </style>";
+            h1 { font-size: 32px; font-weight: normal; }
+            h2 { font-size: 28px; font-weight: normal; }
+            h3 { font-size: 24px; font-weight: normal; }
+            h4 { font-size: 18px; font-weight: normal; }
+            h5 { font-size: 17px; }
+            h6 { font-size: 14px; font-weight: normal; }
+        </style>";
 
         $content = $default_style . $content;
 
